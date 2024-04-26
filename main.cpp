653131eb6467be3329dc4f669a802f168cbb0e21
yuka8888 <133742570+yuka8888@users.noreply.github.com>
2024-04-26 14:29:55 +0900

    コメントを書いた

--- main.cpp
+++ main.cpp
@@ -11,16 +11,20 @@ int main() {
 	unsigned int currentTime = time(nullptr);
 	srand(currentTime);
 
+	//変数に関数を入れる
 	pFunc p = DispResult;
 
 	int num = 0, num2 = rand() % 6 + 1;
 
+	//値の入力
 	printf("半なら1,丁なら2を入力してください\n");
 	scanf_s("%d", &num);
 
+	//関数の呼び出し
 	SetTimeout(p, 5000);
-	printf("サイコロの目は%d", num2);
+	printf("サイコロの目は%d\n", num2);
 
+	//答えの出力
 	if ((num % 2) == (num2 % 2)) {
 		printf("正解\n");
 	}
@@ -31,10 +35,12 @@ int main() {
 	return 0;
 }
 
+//待った時間の出力をする関数
 void DispResult(int* s) {
 	printf("%d秒待った\n", *s / 1000);
 }
 
+//引数の時間分待つ関数
 void SetTimeout(pFunc p, int second) {
 	Sleep(second);
 	p(&second);
