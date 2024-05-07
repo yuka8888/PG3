94dd4ef3bc8fe9c0bd466a01eaeac043f2787219
yuka8888 <133742570+yuka8888@users.noreply.github.com>
2024-05-07 10:17:53 +0900

    変数名の修正

--- main.cpp
+++ main.cpp
@@ -4,24 +4,26 @@
 #include<time.h>
 
 typedef void (*pFunc)(int *);
-void DispResult(int* s);
-void SetTimeout(pFunc p, int second);
+void DispResult(int* second);
+void SetTimeout(pFunc result, int second);
 
 int main() {
-	unsigned int currentTime = time(nullptr);
-	srand(currentTime);
+	unsigned int currentTime = (unsigned int)time(nullptr);	srand(currentTime);
 
 	//変数に関数を入れる
-	pFunc p = DispResult;
+	pFunc result = DispResult;
 
 	int num = 0, num2 = rand() % 6 + 1;
 
-	//値の入力
-	printf("半なら1,丁なら2を入力してください\n");
-	scanf_s("%d", &num);
+	//1か2が入力されるまで無限ループ
+	while (num != 1 && num != 2) {
+		//値の入力
+		printf("半なら1,丁なら2を入力してください\n");
+		scanf_s("%d", &num);
+	}
 
 	//関数の呼び出し
-	SetTimeout(p, 5000);
+	SetTimeout(result, 5000);
 	printf("サイコロの目は%d\n", num2);
 
 	//答えの出力
@@ -36,12 +38,12 @@ int main() {
 }
 
 //待った時間の出力をする関数
-void DispResult(int* s) {
-	printf("%d秒待った\n", *s / 1000);
+void DispResult(int* second) {
+	printf("%d秒待った\n", *second / 1000);
 }
 
 //引数の時間分待つ関数
-void SetTimeout(pFunc p, int second) {
+void SetTimeout(pFunc result, int second) {
 	Sleep(second);
-	p(&second);
+	result(&second);
 }
