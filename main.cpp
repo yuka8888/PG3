e73e6e963853bd5c5eb9180cad64ee8ff513eead
yuka8888 <133742570+yuka8888@users.noreply.github.com>
2024-05-10 14:57:32 +0900

    確認課題03-01

--- main.cpp
+++ main.cpp
@@ -1,54 +1,35 @@
 ﻿#include <stdio.h>
-#include <windows.h>
-#include<stdlib.h>
-#include<time.h>
+#include <list>
 
-typedef void (*pFunc)(int *);
-void DispResult(int* second);
-void SetTimeout(pFunc timer, int second);
+int main(void) {
+	std::list<const char*> yamanoteLine1970{ "Shin-Okubo", "Takadanobaba", "Mejiro",
+"Ikebukuro", "Otsuka", "Sugamo", "Komagome", "Tabata", "Nippori", "Washiya", "Ueno",
+"Okachimachi", "Akihabara", "Kanda", "Tokyo", "Yurakucho", "Shinbashi", "Hamamatsucho", "Tamachi",
+"Shinagawa", "Osaki", "Gotanda", "Meguro", "Ebisu", "Shibuya", "Harajuku", "Yoyogi", "Shinjuku" };
 
-int main() {
-	unsigned int currentTime = (unsigned int)time(nullptr);	srand(currentTime);
-
-	//変数に関数を入れる
-	pFunc dispResult = DispResult;
-
-	int num = 0, num2 = rand() % 6 + 1;
-
-	//1か2が入力されるまで無限ループ
-	while (num != 1 && num != 2) {
-		//値の入力
-		printf("半なら1,丁なら2を入力してください\n");
-		scanf_s("%d", &num);
+	printf("1970年の山手線\n");
+	for (auto itr = yamanoteLine1970.begin(); itr != yamanoteLine1970.end(); ++itr) {
+		printf("%s, ", *itr);
 	}
 
-	//関数の呼び出し
-	SetTimeout(dispResult, 3000);
-	printf("サイコロの目は%d\n", num2);
-
-	//ラムダ式で答えの出力
-	auto result = [=]() {
-		if ((num % 2) == (num2 % 2)) {
-			printf("正解\n");
+	printf("\n2019年の山手線\n");
+	for (auto itr = yamanoteLine1970.begin(); itr != yamanoteLine1970.end(); ++itr) {
+		if (*itr == "Nippori") {
+			itr = yamanoteLine1970.insert(itr, "Nisi-nippori");
+			printf("%s, ", *itr);
+			itr++;
 		}
-		else {
-			printf("不正解\n");
+		printf("%s, ", *itr);
+	}
+
+	printf("\n2022年の山手線\n");
+	for (auto itr = yamanoteLine1970.begin(); itr != yamanoteLine1970.end(); ++itr) {
+		if (*itr == "Shinagawa") {
+			itr = yamanoteLine1970.insert(itr, "Takanawa Gateway");
+			printf("%s, ", *itr);
+			itr++;
 		}
-		};
+		printf("%s, ", *itr);
+	}
 
-	//ラムダ式の呼び出し
-	result();
-
-	return 0;
-}
-
-//待った時間の出力
-void DispResult(int* second) {
-	printf("%d秒待った\n", *second / 1000);
-}
-
-//引数の時間分待つ関数
-void SetTimeout(pFunc timer, int second) {
-	Sleep(second);
-	timer(&second);
 }
